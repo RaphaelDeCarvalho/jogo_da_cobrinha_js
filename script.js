0cd811033bb9b951c2556cf405d40d74175275eb
Raphael de Carvalho <raphaeldecarvalho1@hotmail.com>
2021-05-21 01:29:42 -0300

    desenhado e criado funcao para a aparicao da comida da cobrinha

--- script.js
+++ script.js
@@ -8,6 +8,11 @@ snake[0] = {
 
 }
 let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+
+}
 
 function criarBG() {
     context.fillStyle = "lightgreen";
@@ -23,6 +28,14 @@ function criarCobrinha(){
     }
 
 }
+
+function drawFood(){
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box ,box);
+
+}
+
+
 
 document.addEventListener('keydown', update);
 
@@ -50,6 +63,7 @@ function iniciarJogo(){
 
     criarBG();
     criarCobrinha();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
